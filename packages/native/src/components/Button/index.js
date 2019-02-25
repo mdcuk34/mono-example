870ce5605971f5165943b5774afed75b2d44e858
mdcuk34 <mdcuk34@gmail.com>
2019-02-25 14:57:04 +0000

    added prop-types

--- packages/native/src/components/Button/index.js
+++ packages/native/src/components/Button/index.js
@@ -4,6 +4,7 @@
  */
 
 import React from 'react';
+import PropTypes from 'prop-types';
 import StyledButton from './StyledButton';
 import Wrapper from './Wrapper';
 
@@ -12,5 +13,10 @@ const Button = ({ onPress, children }) => (
     <StyledButton onPress={onPress}>{children}</StyledButton>
   </Wrapper>
 );
+
+Button.propTypes = {
+  onPress: PropTypes.func.isRequired,
+  children: PropTypes.object.isRequired,
+};
 
 export default Button;
