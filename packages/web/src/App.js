64630e6e0d7ef45c78d87b2a044cae4ff82df8be
Mitch <mdcuk34@gmail.com>
2019-02-25 10:26:28 +0100

    Feature/styled-components (#4)
    * added styled components to native
    
    * added styled-components to the web
    
    * added stylelint
    
    * removing css from web/app

--- packages/web/src/App.js
+++ packages/web/src/App.js
@@ -1,29 +1,52 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
-import hello from "@monoexample/shared"
+import React, { Component } from "react";
+import logo from "./logo.svg";
+import styled from "styled-components";
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
+      <Container>
+        <Header>
+          <Logo src={logo} alt="logo" />
           <p>
-            Edit <code>src/App.js</code> and save to reload.{hello}
+            Edit <code>src/App.js</code> and save to reload.
           </p>
-          <a
+          <Link
             className="App-link"
             href="https://reactjs.org"
             target="_blank"
             rel="noopener noreferrer"
           >
             Learn React
-          </a>
-        </header>
-      </div>
+          </Link>
+        </Header>
+      </Container>
     );
   }
 }
 
 export default App;
+
+const Container = styled.div`
+  text-align: center;
+`;
+
+const Logo = styled.img`
+  height: 40vmin;
+  pointer-events: none;
+`;
+
+const Header = styled.header`
+  background-color: #282c34;
+  min-height: 100vh;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  font-size: calc(10px + 2vmin);
+  color: white;
+`;
+
+const Link = styled.a`
+  color: #61dafb;
+`;
