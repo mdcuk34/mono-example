bce0a25aab44dc6d623fd3e34c666d4821d2e5d0
Mitch <mdcuk34@gmail.com>
2019-02-26 08:29:49 +0000

    added reselect (#8)

--- packages/shared/redux/counter/selectors.js
+++ packages/shared/redux/counter/selectors.js
@@ -1,5 +1,8 @@
-function getPreviousCount(count) {
-  return count - 1;
-}
+import { createSelector } from 'reselect';
+
+const getPreviousCount = createSelector(
+  state => state.counter.count,
+  value1 => value1 - 1,
+);
 
 export default { getPreviousCount };
