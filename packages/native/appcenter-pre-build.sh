f3374321416c4fcb34149bf3af21ded2e6071bcd
mdcuk34 <mdcuk34@gmail.com>
2019-03-06 16:32:17 +0000

    pre-build

--- packages/native/appcenter-pre-build.sh
+++ packages/native/appcenter-pre-build.sh
@@ -1,7 +1,3 @@
 #!/usr/bin/env bash
 
-# Example: Change bundle name of an iOS app for non-production
-if [ "$APPCENTER_BRANCH" != "feature/app-centre" ];
-then
-   echo "** PRE BUILD SCRIPT **"
-fi
+echo "** PRE BUILD SCRIPT **"
